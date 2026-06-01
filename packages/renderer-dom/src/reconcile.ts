47c86ac78a0766c5177794ccde0bf124a5c6ac35
Nawin chander <nawinnapzy289@gmail.com>
2026-06-01 16:03:46 +0530

    add

--- packages/renderer-dom/src/reconcile.ts
+++ packages/renderer-dom/src/reconcile.ts
@@ -1,13 +1,15 @@
 export function reconcileChildren (wipFiber:any,elements:any[]){
     let index = 0;
-    let oldFiber=wipFiber.alternate?.child;
-    let prevSibling=null;
+    let oldFiber = wipFiber.alternate?.child;
+    let prevSibling = null;
 
     while(index<elements.length||oldFiber!=null){
         const element = elements[index];
-        let newFiber=null;
-        const sameType=oldFiber&&element&&element.type===oldFiber.type;if(sameType)
-            {newFiber={type:oldFiber.type,props:element.props,dom:oldFiber.dom,parent:wipFiber,alternate:oldFiber,effectTag:'UPDATE'};}
+        let newFiber = null;
+        const sameType= oldFiber && element && element.type===oldFiber.type;
+        if(sameType)
+            {
+                newFiber = {type:oldFiber.type,props:element.props,dom:oldFiber.dom,parent: wipFiber,alternate:oldFiber,effectTag:'UPDATE'};}
         if(element&&!sameType){newFiber={type:element.type,props:element.props,dom:null,parent:wipFiber,
 alternate:null,effectTag:'PLACEMENT'};}
 if(oldFiber&&!sameType){oldFiber.effectTag='DELETION';}
